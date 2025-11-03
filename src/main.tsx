db32d02aeef4156831fb879de4bf93760e43c287
thewronghand <penfreak77@gmail.com>
2025-11-03 22:22:40 +0900

    fix: localStorage 주석을 sessionStorage로 수정 및 불필요한 코드 제거

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { useAuthStore } from './store'
 
-// 앱 시작 시 localStorage에서 토큰 불러오기
+// 앱 시작 시 sessionStorage에서 토큰 불러오기
 useAuthStore.getState().loadFromStorage();
 
 createRoot(document.getElementById('root')!).render(
